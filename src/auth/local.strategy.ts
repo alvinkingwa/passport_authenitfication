561aad7d7d52808959a42149b42c026e5504f925
alvinkingwa <alvin kingwa>
2023-06-15 11:40:37 +0300

    push changes

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -2,7 +2,6 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { retry } from 'rxjs';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { type } from 'os';
 
 export type User = any;
 
 @Injectable()
 export class UsersService {
   private readonly users = [
-    { userId: 1, username: 'alvin', password: 'changename' },
-    { userId: 2, username: 'ndiwa', password: 'changename' },
-    { userId: 3, username: 'kingwa', password: 'changename' },
+    { userId: 1, username: 'alvin', password: 'alvin' },
+    { userId: 2, username: 'ndiwa', password: 'ndiwa' },
+    { userId: 3, username: 'kingwa', password: 'kingwa' },
   ];
 
   async findOne(username: string): Promise<User | undefined> {
